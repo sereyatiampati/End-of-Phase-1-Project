d0d16ffc4eaac0f62e9cbd4b3bbeca6fdce4959c
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-15 07:09:01 +0300

    fetched filtered data

--- src/script.js
+++ src/script.js
@@ -2,3 +2,9 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
   });
 
+  let getText = document.getElementById('search-input').value.trim();
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${getText}`)
+  .then(result=>result.json())
+  .then(data=> {
+      console.log(data)
+  })
