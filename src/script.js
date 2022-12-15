6e201c7df137bae7bbe72e8fab1f8682e5d9c34f
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-15 14:06:18 +0300

    added a form event listener

--- src/script.js
+++ src/script.js
@@ -2,9 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
   });
 
-  let getText = document.getElementById('search-input').value.trim();
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${getText}`)
-  .then(result=>result.json())
-  .then(data=> {
-      console.log(data)
-  })
+const form= document.querySelector('form')
+const results= document.querySelector('.food-result')
+const container= document.querySelector('.container')
+let searchQuery= "";
+form.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  searchQuery = e.target.querySelector('input').value;
+  console.log(searchQuery)
+})
