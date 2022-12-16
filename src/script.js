b9b65defa542cde724ba49bea748c2ea7dbd41a3
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-16 07:31:01 +0300

    added an event listener to the search button

--- src/script.js
+++ src/script.js
@@ -2,12 +2,54 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
   });
 
-const form= document.querySelector('form')
-const results= document.querySelector('.food-result')
-const container= document.querySelector('.container')
-let searchQuery= "";
-form.addEventListener("submit", (e)=> {
+const container = document.querySelector('.container')
+const resultsDiv = document.querySelector('.food-result')
+const form = document.querySelector('form')
+const searchButton = document.getElementById('search-btn')
+
+
+//event listeners
+searchButton.addEventListener("click", (e)=> {
   e.preventDefault();
-  searchQuery = e.target.querySelector('input').value;
-  console.log(searchQuery)
+  showFoodResult()
 })
+
+//show results that match the input from user
+function showFoodResult(){
+  let searchQuery = document.getElementById('search-input').value.trim();
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchQuery}`)
+  .then(response => response.json())
+  .then(results => {
+    console.log(results)
+    let template="";
+    if (results.meals){
+      console.log(results.meals)
+      results.meals.forEach(foodItem=> {
+        
+        template+=`
+        <div class="food-item" data-id="${foodItem.idMeal}">
+          <img src="${foodItem.strMealThumb}" alt="">
+          <div class="food-details">
+            <h3 class="food-name">${foodItem.strMeal}</h3>
+            <h4 class = "ingredient-category">Category Name</h4>
+            <div class = "recipe-instructions">
+              <h3>Instructions:</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo blanditiis quis accusantium natus! Porro, reiciendis maiores molestiae distinctio veniam ratione ex provident ipsa, soluta suscipit quam eos velit autem iste!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam voluptatibus ad obcaecati magnam, esse numquam nisi ut adipisci in?</p>
+            </div>
+          </div>
+        </div>
+        `;
+      });
+
+       resultsDiv.classList.remove('noResult')
+
+    } 
+     else {
+      template = "Sorry, No Results, try another ingredient!"
+      resultsDiv.classList.add('noResult')
+      
+    }
+    resultsDiv.innerHTML=template;
+  })
+}
