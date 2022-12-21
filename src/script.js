87eba37215350c9b629d445a0990223bba9bd8aa
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-22 01:42:54 +0300

    edited the no result message displayed to the DOM

--- src/script.js
+++ src/script.js
@@ -25,17 +25,17 @@ function showFoodResult(){
     if (results.meals){
       console.log(results.meals)
       results.meals.forEach(foodItem=> {
+        let category= searchQuery[0].toUpperCase()+searchQuery.substring(1)
         
         template+=`
         <div class="food-item" data-id="${foodItem.idMeal}">
           <img src="${foodItem.strMealThumb}" alt="">
           <div class="food-details">
             <h3 class="food-name">${foodItem.strMeal}</h3>
-            <h4 class = "ingredient-category">Category Name</h4>
+            <h4 class = "ingredient-category">${category}</h4>
             <div class = "recipe-instructions">
               <h3>Instructions:</h3>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo blanditiis quis accusantium natus! Porro, reiciendis maiores molestiae distinctio veniam ratione ex provident ipsa, soluta suscipit quam eos velit autem iste!</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam voluptatibus ad obcaecati magnam, esse numquam nisi ut adipisci in?</p>
+              <p>Heat oven to 160C/fan 140C/gas 3. Heat some dripping or butter in a large shallow casserole dish, brown the lamb in batches, lift to a plate, then repeat with the kidneys.Fry the onions and carrots in the pan with a little more dripping until golden."</p>
             </div>
           </div>
         </div>
@@ -48,7 +48,7 @@ function showFoodResult(){
      else {
       template = "Sorry, No Results, try another ingredient!"
       resultsDiv.classList.add('noResult')
-      
+
     }
     resultsDiv.innerHTML=template;
   })
