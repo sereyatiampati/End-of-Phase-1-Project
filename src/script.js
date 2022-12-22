144c6920a4725cd885892a6c84d8e7dd66bb53b4
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-22 19:22:01 +0300

    added comments

--- src/script.js
+++ src/script.js
@@ -2,11 +2,8 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
   });
 
-const container = document.querySelector('.container')
 const resultsDiv = document.querySelector('.food-result')
-const form = document.querySelector('form')
 const searchButton = document.getElementById('search-btn')
-
 
 //event listeners
 searchButton.addEventListener("click", (e)=> {
@@ -14,10 +11,10 @@ searchButton.addEventListener("click", (e)=> {
   showFoodResult()
 })
 
-//show results that match the input from user
+//show results that match the (search) input from user
 function showFoodResult(){
   let searchQuery = document.getElementById('search-input').value.trim();
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchQuery}`)
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`)
   .then(response => response.json())
   .then(results => {
     console.log(results)
@@ -25,17 +22,17 @@ function showFoodResult(){
     if (results.meals){
       console.log(results.meals)
       results.meals.forEach(foodItem=> {
-        let category= searchQuery[0].toUpperCase()+searchQuery.substring(1)
-        
+        //food items to be appended to the result section if the search query match 
         template+=`
         <div class="food-item" data-id="${foodItem.idMeal}">
           <img src="${foodItem.strMealThumb}" alt="">
           <div class="food-details">
+            <h4 class = "food-origin">${foodItem.strArea} Food</h4>
             <h3 class="food-name">${foodItem.strMeal}</h3>
-            <h4 class = "ingredient-category">${category}</h4>
+            <h4 class = "ingredient-category">${foodItem.strCategory}</h4>
             <div class = "recipe-instructions">
               <h3>Instructions:</h3>
-              <p>Heat oven to 160C/fan 140C/gas 3. Heat some dripping or butter in a large shallow casserole dish, brown the lamb in batches, lift to a plate, then repeat with the kidneys.Fry the onions and carrots in the pan with a little more dripping until golden."</p>
+              <p>${foodItem.strInstructions}</p>
             </div>
           </div>
         </div>
