75d65ce340e763504c12c8b5ddd152e5118f2a65
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-24 13:39:26 +0300

    added a contentloaded event listener

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+    showFoodResult()
   });
 
 const resultsDiv = document.querySelector('.food-result')
@@ -11,7 +12,7 @@ searchButton.addEventListener("click", (e)=> {
   showFoodResult()
 })
 
-//show results that match the (search) input from user
+//show results that match the (search-query) input from user
 function showFoodResult(){
   let searchQuery = document.getElementById('search-input').value.trim();
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`)
@@ -22,14 +23,14 @@ function showFoodResult(){
     if (results.meals){
       console.log(results.meals)
       results.meals.forEach(foodItem=> {
-        //food items to be appended to the result section if the search query match 
+        //food items to be appended to the result section if the search query match any meals 
         template+=`
         <div class="food-item" data-id="${foodItem.idMeal}">
-          <img src="${foodItem.strMealThumb}" alt="">
+          <img src="${foodItem.strMealThumb}" alt="${foodItem.strMeal} Recipe">
           <div class="food-details">
-            <h4 class = "food-origin">${foodItem.strArea} Food</h4>
+            <h4 class = "food-origin">${foodItem.strArea}</h4>
             <h3 class="food-name">${foodItem.strMeal}</h3>
-            <h4 class = "ingredient-category">${foodItem.strCategory}</h4>
+            <h4 class = "food-category">${foodItem.strCategory}</h4>
             <div class = "recipe-instructions">
               <h3>Instructions:</h3>
               <p>${foodItem.strInstructions}</p>
